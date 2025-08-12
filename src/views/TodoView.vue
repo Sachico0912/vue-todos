238627482e7996474e07eb04693f395fb00879a2
Sachico <s104103045@gmail.com>
2025-08-12 10:15:21 +0800

    fix: debug

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -4,37 +4,37 @@ import axios from 'axios'
 
 const api = 'https://todolist-api.hexschool.io/'
 
-const signupField = ref({
+const signUpField = ref({
   email: '',
   password: '',
   nickname: '',
 })
 
-const signupRes = ref('')
+const signUpRes = ref('')
 
-const signup = async () => {
+const signUp = async () => {
   try {
     //   console.log(`${api}users/signup`)
-    const res = await axios.post(`${api}users/sign_up`, signupField.value)
+    const res = await axios.post(`${api}users/sign_up`, signUpField.value)
     console.log(res)
-    signupRes.value = res.data.uid
+    signUpRes.value = res.data.uid
   } catch (error) {
     console.error('註冊失敗:', error)
   }
 }
 
-const signinField = ref({
+const signInField = ref({
   email: '',
   password: '',
 })
 
-const signinRes = ref('')
+const signInRes = ref('')
 
 const signIn = async () => {
   try {
-    const res = await axios.post(`${api}users/sign_in`, signinField.value)
+    const res = await axios.post(`${api}users/sign_in`, signInField.value)
     console.log(res)
-    signinRes.value = res.data.token
+    signInRes.value = res.data.token
   } catch (error) {
     console.error('登入失敗:', error)
   }
@@ -43,21 +43,23 @@ const signIn = async () => {
 <template>
   <h2>待辦</h2>
   <h2>註冊功能</h2>
-  <input type="email" placeholder="電子郵件" v-model="signupField.email" /><br />
-  <input type="password" placeholder="密碼" v-model="signupField.password" /><br />
-  <input type="text" placeholder="暱稱" v-model="signupField.nickname" /><br />
-  <button type="button" @click="signup">註冊</button><br />
-  {{ signupField }}
+  <input type="email" placeholder="電子郵件" v-model="signUpField.email" /><br />
+  <input type="password" placeholder="密碼" v-model="signUpField.password" /><br />
+  <input type="text" placeholder="暱稱" v-model="signUpField.nickname" /><br />
+  <button type="button" @click="signUp">註冊</button><br />
+  {{ signUpField }}
+  <hr />
+  <b>Uid:{{ signUpRes }}</b>
 
   <hr />
   <br />
   <h2>登入功能</h2>
-  <input type="email" placeholder="電子郵件" v-model="signinField.email" />
-  <input type="password" placeholder="密碼" v-model="signinField.password" />
+  <input type="email" placeholder="電子郵件" v-model="signInField.email" />
+  <input type="password" placeholder="密碼" v-model="signInField.password" />
   <button type="button" @click="signIn">登入</button>
-  {{ signinField }}
+  {{ signInField }}
   <hr />
-  <b>token: {{ signinRes }}</b>
+  <b>token: {{ signInRes }}</b>
   <br />
   <h2>登出功能</h2>
   <button type="button">登出</button>
