fa3097792506d2b444fc113199d27347da6e2a02
Sachico <s104103045@gmail.com>
2025-08-26 14:37:38 +0800

    feat:componenet card

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<!-- 卡片元件設計 defineProps() -->
+
+<script setup>
+const props = defineProps({
+  title: { type: String, required: true },
+})
+</script>
+
+<template>
+  <div style="border: 1px solid #ddd; padding: 8px; border-radius: 6px">
+    <h3>{{ props.title }}</h3>
+  </div>
+</template>
--- src/views/Week3View.vue
+++ src/views/Week3View.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Card from '../components/Card.vue'
+const pageTitle = '卡片標題'
+</script>
 
 <template>
   <h1>Week3 page</h1>
+  <Card :title="pageTitle" />
 </template>
 
 <style></style>
